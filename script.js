7fdf00657850a11aa1dc096fb8f08ab733a08c0e
pedroxavier1451 <129323635+pedroxavier1451@users.noreply.github.com>
2023-04-06 08:42:04 -0500

    4 files modified

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ console.log('Hello!');
 
 function cambiarColor() {
   var texto = document.getElementById("miTexto");
-  texto.style.color = "blue";
+  var texto2 = document.getElementById("miTexto2");
+  var texto3 = document.getElementById("miTexto3");
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  texto.style.color = "rgb(" + r + "," + g + "," + b + ")";
+  texto2.style.color = "rgb(" + r + "," + g + "," + b + ")";
+  texto3.style.color = "rgb(" + r + "," + g + "," + b + ")";
 }
+
+setInterval(cambiarColor, 2000);
 	
